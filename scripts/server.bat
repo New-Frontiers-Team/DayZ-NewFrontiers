345df570a506daed8e13a6bfc474b809b381469e
Frooszy <thecryzin@gmail.com>
2023-07-24 12:11:54 -0300

    COT added to test server

--- scripts/server.bat
+++ scripts/server.bat
@@ -1,2 +1,2 @@
 :: Start test server with mods
-start /D "%DAYZ_FOLDER%" DayZDiag_x64.exe "-mod=P:\@CF;P:\@NewFrontiers;P:\@NewFrontiersBases" -filePatching -server -config=serverDZ.cfg
+start /D "%DAYZ_FOLDER%" DayZDiag_x64.exe "-mod=P:\@CF;%DAYZ_FOLDER%\!Workshop\@Community-Online-Tools;P:\@NewFrontiers;P:\@NewFrontiersBases" -filePatching -server -config=serverDZ.cfg
