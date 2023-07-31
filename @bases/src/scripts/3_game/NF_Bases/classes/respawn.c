1385fd7de6ac0ac5a7131124dfc41d7087fc36f5
Tiago Damascena <tiagobdamascena@gmail.com>
2023-07-31 01:08:41 -0300

    Migrated some classes from game to world module

--- @bases/src/scripts/3_game/NF_Bases/classes/respawn.c
+++ @bases/src/scripts/3_game/NF_Bases/classes/respawn.c
@@ -3,30 +3,3 @@ class NF_Bases_RespawnData
     string position;
     int radius;
 }
-
-class NF_Bases_Respawn
-{
-    private vector m_position;
-	private int m_radius;
-
-    void NF_Bases_Respawn(NF_Bases_RespawnData data)
-    {
-        m_position = data.position.ToVector();
-        m_radius = data.radius;
-    }
-
-    vector GetPosition()
-    {
-        return m_position;
-    }
-
-    int GetRadius()
-    {
-        return m_radius;
-    }
-
-	vector GetRandomPosition()
-	{
-		return NF_Vector.RandomPositionOnRadius(m_position, m_radius);
-	}
-}
--- @bases/src/scripts/3_game/NF_Bases/classes/safezone.c
+++ @bases/src/scripts/3_game/NF_Bases/classes/safezone.c
@@ -3,38 +3,3 @@ class NF_Bases_SafezoneData
     string position;
     int radius;
 }
-
-class NF_Bases_Safezone
-{
-    private int m_faction;
-    private vector m_position;
-	private int m_radius;
-
-    void NF_Bases_Safezone(NF_Bases_SafezoneData data, int faction)
-    {
-        m_faction = faction;
-        m_position = data.position.ToVector();
-        m_radius = data.radius;
-    }
-
-    int GetFaction()
-    {
-        return m_faction;
-    }
-
-    vector GetPosition()
-    {
-        return m_position;
-    }
-
-    int GetRadius()
-    {
-        return m_radius;
-    }
-
-    bool IsInside(vector targetPosition)
-    {
-        float distance = NF_Vector.Distance2D(m_position, targetPosition);
-        return distance <= m_radius;
-    }
-}
