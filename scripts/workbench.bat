e777bf1ac7d8301509e96c2bd37f47908fab16fb
Frooszy <thecryzin@gmail.com>
2024-04-26 12:32:27 -0300

    Base of Squad mod

--- scripts/workbench.bat
+++ scripts/workbench.bat
@@ -1,4 +1,4 @@
 :: Start Enfusion Workbench with mods
 @echo off
 
-start /D "%DAYZ_TOOLS%\Bin\Workbench" workbenchApp.exe "-mod=P:\@CF;P:\@NewFrontiers;P:\@NewFrontiersBases;P:\@NewFrontiersKillFeed"
+start /D "%DAYZ_TOOLS%\Bin\Workbench" workbenchApp.exe "-mod=P:\@CF;P:\@NewFrontiers;P:\@NewFrontiersBases;P:\@NewFrontiersKillFeed;P:\@NewFrontiersSquad"
